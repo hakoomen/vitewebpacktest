b1943014d91d737c22bd4359792f303b13bb1227
m-setayesh-m <m-setayesh-m@agri-bank.com>
2023-07-16 09:43:58 +0330

    update to be the same as webpack

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,17 +1,22 @@
-import * as Extra from "./extra"
-import {useLess} from "./extra"
+//export * as Extra from "./extra"; // will be dropped as dead code unless we mark it as a sideEffect
+//export {useLess} from "./extra"; // will be dropped as dead code unless we mark it as a sideEffect
+import "./extra"; //  sideEffect only import (this should work no matter what but in webpack sideEffects property in package.json overrides the language behavior which is horrible)
+//  this is still an open issue on their github: https://github.com/webpack/webpack/issues/6571
+// REMEMBER TO CHANGE PACKAGE.JSON SIDE-EFFECTS
 
-export enum LogEvel {
+console.log("utils-side-effect-are");
+
+export enum LogLevel {
   ERROR,
   INFORMATION,
 }
 
-export function log(message: string, logLevel: LogEvel = LogEvel.INFORMATION) {
+export function log(message: string, logLevel: LogLevel = LogLevel.INFORMATION) {
   switch (logLevel) {
-    case LogEvel.ERROR:
+    case LogLevel.ERROR:
       console.error(message);
       break;
-    case LogEvel.INFORMATION:
+    case LogLevel.INFORMATION:
       console.info(message);
       break;
   }
