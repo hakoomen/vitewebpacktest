6ba760caff41b60d79330e00a7f53a291ef2084d
m-setayesh-m <m-setayesh-m@agri-bank.com>
2023-07-17 16:03:59 +0330

    tmp for chunk

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,10 @@
-import {log} from "./utils";
+import { log } from "./utils";
 import "scss/index";
 
 log("top-level-statement");
+
+document.addEventListener("DOMContentLoaded", () => {
+    import(/* */"chart.js").then((chart) => {
+        console.log("doing sth with chart module");
+    });
+});
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,44 +2,63 @@ import * as path from "path";
 import { Configuration } from "webpack";
 
 const config: Configuration = {
-  entry: "./src/ts/index.ts",
-  mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-        exclude: /node_modules/
-      },
-    ]
-  },
-  resolve: {
-    // modify how imports work
-    alias: {
-      scss: ["/src/scss"],
+    entry: {
+        bundle: "./src/ts/index.ts",
     },
-    extensions: [".ts", ".js", ".scss"], // omit extension in imports(if multiple files exist by the same name a.js and a.ts it will go for a.ts because its preceding .js in the list)
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization:{
-    usedExports: true
-  },
-
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        // modify how imports work
+        alias: {
+            scss: ["/src/scss"],
+        },
+        extensions: [".ts", ".js", ".scss"], // omit extension in imports(if multiple files exist by the same name a.js and a.ts it will go for a.ts because its preceding .js in the list)
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "dist/",
+    },
+    optimization: {
+        usedExports: true,
+        splitChunks: {
+            chunks: "all",
+            minSize: 5,
+            minRemainingSize: 0,
+            minChunks: 1,
+            minSizeReduction: 0,
+            maxInitialRequests: Infinity,
+            // cacheGroups: {
+            //     initial: {
+            //         name: "initial",
+            //         chunks: "initial",
+            //         minChunks: 2,
+            //         test: /src/,
+            //         priority: -5,
+            //         reuseExistingChunk: true,
+            //     },
+            // },
+        },
+    },
 };
 
 export default config;
