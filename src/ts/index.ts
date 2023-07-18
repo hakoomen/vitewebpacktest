f6703c2e60ccb4d815a0a758007010d6faf9f11d
m-setayesh-m <m-setayesh-m@agri-bank.com>
2023-07-18 10:12:59 +0330

    demo

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,14 @@
 import { log } from "./utils";
 import "scss/index";
 
+
 log("top-level-statement");
 
 document.addEventListener("DOMContentLoaded", () => {
-    import(/* */"chart.js").then((chart) => {
+    import("chart.js").then((chart) => {
         console.log("doing sth with chart module");
     });
 });
+
+
+
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,9 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import * as path from "path";
 import { Configuration } from "webpack";
 
 const config: Configuration = {
-    entry: {
-        bundle: "./src/ts/index.ts",
-    },
+    entry: "./src/ts/index.ts",
     mode: "production",
     module: {
         rules: [
@@ -15,50 +14,35 @@ const config: Configuration = {
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
+                use: ["style-loader", "css-loader", "sass-loader"],
                 exclude: /node_modules/,
             },
         ],
     },
+
     resolve: {
-        // modify how imports work
         alias: {
             scss: ["/src/scss"],
         },
-        extensions: [".ts", ".js", ".scss"], // omit extension in imports(if multiple files exist by the same name a.js and a.ts it will go for a.ts because its preceding .js in the list)
+        extensions: [".ts", ".js", ".scss"],
     },
     output: {
+        filename: "bundle.[contenthash].js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "dist/",
     },
     optimization: {
-        usedExports: true,
+        runtimeChunk: "single",
         splitChunks: {
             chunks: "all",
-            minSize: 5,
-            minRemainingSize: 0,
-            minChunks: 1,
-            minSizeReduction: 0,
-            maxInitialRequests: Infinity,
-            // cacheGroups: {
-            //     initial: {
-            //         name: "initial",
-            //         chunks: "initial",
-            //         minChunks: 2,
-            //         test: /src/,
-            //         priority: -5,
-            //         reuseExistingChunk: true,
-            //     },
-            // },
+            minSize: 0,
         },
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./index.html"),
+            filename: "index.html",
+        }),
+    ],
 };
 
 export default config;
